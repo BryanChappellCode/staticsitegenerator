acd514519fe371a340b7b1d173f8454488d6ee9a
BryanChappellCode <bryanchappellcode@gmail.com>
2024-10-03 17:16:16 -0500

    Fixed Textnode

--- src/textnode.py
+++ src/textnode.py
@@ -21,16 +21,7 @@ class TextNode:
 
         return True
     
-    def text_node_to_html_node(self):
-
-        if self.text_type == text_type_text: return LeafNode(value=self.text)
-        if self.text_type == text_type_bold: return LeafNode("b", self.text)
-        if self.text_type == text_type_italic: return LeafNode("i", self.text)
-        if self.text_type == text_type_code: return LeafNode("code", self.text)
-        if self.text_type == text_type_link: return LeafNode("a", self.text, {"href": self.url})
-        if self.text_type == text_type_image: return LeafNode("img", "", {"href": self.url, "alt": self.text})
-
-        raise ValueError("Invalid text node type")
+    
 
     def __repr__(self):
 
